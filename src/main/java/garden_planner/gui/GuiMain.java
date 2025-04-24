72ab0d37831f7d43c4171ce397a2eaa3024fb3c7
mandaza <prospermandazat@gmail.com>
2025-04-24 20:02:38 +1000

    Week-7

--- src/main/java/garden_planner/gui/GuiMain.java
+++ src/main/java/garden_planner/gui/GuiMain.java
@@ -7,8 +7,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -19,13 +21,54 @@ import javafx.stage.Stage;
  */
 public class GuiMain extends Application {
 
+    // 1-1 association with GardenPlanner
+    private GardenPlanner planner;
+    private TextField widthField;
+
+    // Constructor: set up planner and default layout
+    public GuiMain(){
+        planner = new GardenPlanner();
+        planner.createBasicDesign();
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //Scale factor to convert meters to pixels
+        final double SCALE = 100.0;
+
+        //Create the main layout as a BorderPane
+        BorderPane root = new BorderPane();
+
+        //Create the garden layout area using a Pane
+        Pane gardenPane = new Pane();
+        root.setStyle("-fx-background-color: #007700;");
+        root.setCenter(gardenPane);
+
+
+        //Loop through all garden beds and add them as rectangles to the GUI
+        for(RectBed bed : planner.getBeds()){
+            Rectangle rect = new Rectangle(bed.getWidth() * SCALE, bed.getHeight() * SCALE);
+            rect.setX(bed.getLeft() * SCALE);
+            rect.setY(bed.getTop() * SCALE);
+            rect.setFill(Color.SADDLEBROWN);
+            rect.setStroke(Color.BLACK);
+            root.getChildren().add(rect);
+        }
+
+
+        //Create and configure TextField to display width
+        widthField = new TextField("???");
+        root.setBottom(widthField);
+
+        //Set width of first be into the text field
+        double width = planner.getBeds().get(0).getWidth();
+        String str = Double.toString(width);
+        widthField.setText(str);
+
+
         // Parent root = FXMLLoader.load(getClass().getResource("garden_gui.fxml"));
-        Button root = new Button("TODO: design garden planner GUI");
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setTitle("Garden Planner");
         primaryStage.show();
     }
 
